7b32ba29b70c6e5797105ced96a04a363d646b62
Зинаков Никита Андреевич <n.zinakov@yandex.ru>
2024-02-28 04:38:01 +0700

    Добавил ответ на задание 7. В задании нужно было написать калькулятор, после чего каждую операцию разбить на модули и создать из всего этого дела библиотеку. Цельная программа лежит в папке "old"

---
+++ hw7_StaticCalc/calculator.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+float isValid(float number);
+float plus(float x, float y);
+float minus(float x, float y);
+float multiply(float x, float y);
+float divide(float x, float y);
+
+#endif
